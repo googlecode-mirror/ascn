0f0d5e9b7b2043e9855bd3e2c01f27753d7c4ea4
julien.maulny@gmail.com <julien.maulny@gmail.com@b69641be-e6fc-d46c-bffd-0a71ad014cef>
2012-04-05 22:49:15 +0000

    Terminer Jeu en cours, terminer tic tac toe en cours...

--- trunk/www/games/tictactoe/TicTacToe.php
+++ trunk/www/games/tictactoe/TicTacToe.php
@@ -52,27 +52,38 @@ class TicTacToe extends Jeu {
 	public function ajax_kik() {
 		$this->loadData();
 		
+		
+		list($nb_red, $nb_blue)=self::compterSignes($this->grille);
+		
 		$x=intval(getValue('x'));
 		$y=intval(getValue('y'));
 		
 		$charat=($y*3)+$x;
-		
-		$nb_red=0;
-		$nb_blue=0;
-		for($i=0;$i<9;$i++) {
-			switch($this->grille[$i]) {
-				case 1: $nb_red++; break;
-				case 2: $nb_blue++;break;
-			}
-		}
 		
 		if($this->grille[$charat]==0 && (($nb_red-$nb_blue+1)==intval(slot()->position))) {
 			$this->grille[$charat]=intval(slot()->position);
 			$this->saveData();
 		}
 		
-		return $this->ajax_update();
+		
+		switch($winner=self::checkTicTacToe($this->grille)) {
+			case 0:
+				return $this->ajax_update();
+				
+			case 1:case 2:
+				$slots=partie()->getSlots();
+				$slots[$winner-1]->addScore(1);
+				$this->terminer();
+				return new AJAXResponse();
+				break;
+				
+			default:
+				throw new Exception('grille de Tic Tac Toe impossible');
+		}
+		
+		
 	}
+	
 	
 	
 	
@@ -98,6 +109,42 @@ class TicTacToe extends Jeu {
 		partie()->save();
 	}
 	
+	
+	/**
+	 * 
+	 * Enter description here ...
+	 * @param Array $grille a évaluer. Doit être un array de 9 int = 0:vide, 1:joueur1 ou 2:joueur2
+	 * @return integer :
+	 * 				0 : pas fini
+	 * 				1 : J1 win
+	 * 				2 : J2 win
+	 * 				-1: Erreur : grille impossible
+	 */
+	public static function checkTicTacToe($grille) {
+		
+		// Regarde si un joueur à joué plus que l'autre
+		list($nb_red, $nb_blue)=self::compterSignes($this->grille);
+		$diff=$nb_red-$nb_blue;
+		if($diff<-1 || $diff>1) return -1;
+		
+		
+		
+		return 0;
+	}
+	
+	
+	private static function compterSignes($grille) {
+		$nb_red=0;
+		$nb_blue=0;
+		for($i=0;$i<9;$i++) {
+			switch($grille[$i]) {
+				case 1: $nb_red++; break;
+				case 2: $nb_blue++;break;
+			}
+		}
+		
+		return array($nb_red, $nb_blue);
+	}
 	
 	
 }
