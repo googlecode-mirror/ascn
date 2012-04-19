864fa45d8c848d45ccbb005d95ac49e98940644b
julien.maulny@gmail.com <julien.maulny@gmail.com@b69641be-e6fc-d46c-bffd-0a71ad014cef>
2012-04-19 21:31:32 +0000

    avancée Dammes (parametres)

--- trunk/www/games/dammes/Dammes.php
+++ trunk/www/games/dammes/Dammes.php
@@ -5,33 +5,53 @@
 class Dammes extends Jeu {
 	
 	private $taille_plateau;
-	
+	private $peut_manger_en_arriere;
+	private $damme_deplacement_long;
 	
 	
 	
 	
 	public function process() {
+		$this->setRegles();
 		$this->addJs(WWW_JS.'draggable.min.js');
 		smarty()->assign('plateau_inverse', false);
+		
+		$demi=$this->taille_plateau/2;
+		$nb_pion=$demi*($demi-1);
+		
+		smarty()->assign($this->getArrayParam());
 	}
 	
 	
 	public function getInitialData() {
+		$this->setRegles();
 		$data->tours=new Tours(partie()->option('premier_joueur'));
 		$data->partie_data->cases=array();
+		$data->param=$this->getArrayParam();
 		
-		for($i=0;$i<10;$i++) {
+		for($i=0;$i<8;$i++) {
 			$line=array();
-			for($j=0;$j<10;$j++) {
+			for($j=0;$j<8;$j++) {
 				$line[]=0;
 			}
 			$data->partie_data->cases[]=$line;
 		}
 		
-		for($i=0;$i<20;$i++) {
-			$data->partie_data->cases[(int)(($i*2)/10)][(int)(($i*2)%10)]=1;
-			$data->partie_data->cases[9-(int)(($i*2+1)/10)][9-(int)(($i*2+1)%10)]=2;
+		$t=$this->taille_plateau;
+		$demi=$t/2;
+		$nb_pion=$demi*($demi-1);
+		
+		for($i=0;$i<($demi-1);$i++) {
+			for($j=0;$j<$demi;$j++) {
+				$x=$i;
+				$y=$j*2+$i%2;
+				julog(print_r(array($x,$y),true));
+				
+				$data->partie_data->cases[$x][$y]=1;
+				$data->partie_data->cases[$t-$x-1][$t-$y-1]=2;
+			}
 		}
+		
 		
 		return $data;
 	}
@@ -39,9 +59,23 @@ class Dammes extends Jeu {
 	
 	
 	public function setRegles() {
-		
+		// switch(partie()->option('regles')) { ... }
+		$this->taille_plateau=8;
+		$this->peut_manger_en_arriere=false;
+		$this->damme_deplacement_long=false;
 	}
 	
 	
+	public function getArrayParam() {
+		$demi=$this->taille_plateau/2;
+		$nb_pion=$demi*($demi-1);
+		return array(
+			'taille_plateau'			=> $this->taille_plateau,
+			'peut_manger_en_arriere'	=> $this->peut_manger_en_arriere,
+			'damme_deplacement_long'	=> $this->damme_deplacement_long,
+			'nb_pion'					=> $nb_pion,
+			'demi'						=> $demi,
+		);
+	}
 	
 }
--- trunk/www/games/dammes/dammes.tpl
+++ trunk/www/games/dammes/dammes.tpl
@@ -1,15 +1,15 @@
 
 
-<div id="plateau" class="std-plateau">
-	{section name=loop_x start=0 loop=10 step=1}
-		{section name=loop_y start=0 loop=10 step=1}
+<div id="plateau" class="std-plateau" style="width:{$taille_plateau*64}px;height:{$taille_plateau*64}px;">
+	{section name=loop_x start=0 loop=$taille_plateau step=1}
+		{section name=loop_y start=0 loop=$taille_plateau step=1}
 		
 			{if !$plateau_inverse}
 				{assign var='x' value=$smarty.section.loop_x.index}
 				{assign var='y' value=$smarty.section.loop_y.index}
 			{else}
-				{assign var='x' value=9-$smarty.section.loop_x.index}
-				{assign var='y' value=9-$smarty.section.loop_y.index}
+				{assign var='x' value=$taille_plateau-1-$smarty.section.loop_x.index}
+				{assign var='y' value=$taille_plateau-1-$smarty.section.loop_y.index}
 			{/if}
 			{assign var='parite' value=(($x%2)==($y%2))}
 			
@@ -31,7 +31,7 @@
 				{assign var='couleur' value='noir'}
 			{/if}
 			
-			{section name=pion start=0 loop=20 step=1}
+			{section name=pion start=0 loop=$nb_pion step=1}
 				{assign var='pion' value=$smarty.section.pion.index}
 				
 				<div id="pion-{$joueur}-{$pion}" class="std-pion pion-{$couleur} draggable"></div>
